0790e91fb90a0e6ec01e7a578c3eb9210850c288
Denise Schleier <denise.schleier@outlook.com>
2024-03-02 10:07:34 -0800

    server

---
+++ server.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 5000
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = createServer(async (req, res) => {
+        //console.log(req.url)
+
+        try {
+            await handle(req, res)
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err)
+            res.statusCode = 500
+            res.end('internal server error')
+        }
+    })
+
+    server.once('error', (err: any) => {
+        console.error(err)
+        process.exit(1)
+    })
+    server.listen(port, () => {
+        console.log(`> Ready on http://${hostname}:${port}`)
+    })
+})
